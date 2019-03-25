540ed9fd6ff3c43ac33793556d94128a4b1ccd9a
rotsoc <socrtjin@gmail.com>
2019-03-25 21:23:37 +0200

    finish exercise 2

--- exercise2.py
+++ exercise2.py
@@ -24,7 +24,30 @@ def main():
     print(f"The xplot is {xplot}")
     print(f"The yplot is {yplot}")
 
-    plt.plot(xplot,yplot)
+    s = sum(1/sqrt(i) for i in range(1, 201))
+
+    print(f"The sum is {s}")
+
+    a = -0.5
+    h = 1/1000
+    u = [exp(0), exp(h*a), exp(2*h*a)]
+
+    for n in range(3, 1001):
+        u.append(u[n-1]+h*a*((23/12)*u[n-1]-(4/3)*u[n-2]+(5/12)*u[n-3]))
+
+    print(f"u is {u}")
+
+    td = [n*h for n in range(0, 1001)]
+
+    print(f"time is {td}")
+
+    plt.plot(u, td, 'd')
+
+    yplot = [exp(a*tt) for tt in td]
+
+    plt.plot(yplot, td)
+
+    plt.show()
     
 
 
